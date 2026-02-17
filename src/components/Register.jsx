38822f53a4f138e982f44e3732179aa5a7b4eaaf
kaus018 <kkad018@mail.ru>
2026-02-17 14:50:38 +0500

    registration

---
+++ src/components/Register.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const userData = { username, password };
+    localStorage.setItem('user', JSON.stringify(userData));
+    navigate('/login');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default Register;
